0bcc9f0cdcd6965d54af559c0d5e4e6176e7f9e6
Anil Kumar <kumarglci@gmail.com>
2025-09-08 16:34:08 +0530

    Deploy website - based on d33a007bf7f9aba91f6c696040206e45b901282d

--- src/components/Highlights/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Highlight({ children, className }) {
-    return (
-        <span className={className}>
-            {children}
-        </span>
-    )
-};
